3ef99506b04451f4560e6757e36ad2913f7b4ebd
vivek9patel <vivek.p9737@gmail.com>
2021-06-29 18:59:28 +0530

    disables extension on button click

--- background.js
+++ background.js
@@ -1,6 +1,24 @@
+let isExtensionActivated = false;
+
+chrome.storage.local.get(["activate-extenstion"], (res) => {
+  isExtensionActivated = (res["activate-extenstion"] === "false")
+});
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  try {
+    const value = changes["activate-extenstion"].newValue;
+    isExtensionActivated = (value === "false")
+  }
+  catch { }
+});
+
 const toggleMuteState = async (tab, muted) => {
   console.log(`Tab ${tab.title} requested to ${muted ? 'mute' : 'unmute'}`);
-  if (muted === tab.mutedInfo.muted) {
+  if (!isExtensionActivated) {
+    console.log(`Extension is Deactivated!`);
+    return;
+  }
+  else if (muted === tab.mutedInfo.muted) {
     console.log(`Tab ${tab.title} already ${muted ? 'mute' : 'unmute'}`);
     return;
   }
