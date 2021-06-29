135e29b0490f718efdbee3ecfcf25ffe9e605a10
vivek9patel <vivek.p9737@gmail.com>
2021-06-29 14:52:43 +0530

    adds activate/deactivate button in popup

--- popup.js
+++ popup.js
@@ -1,3 +1,17 @@
-document.getElementById("mute-spotify").addEventListener("click", () => {
-    chrome.tabs.sendMessage({ msg: "muteSpotify" });
+const muteButton = document.getElementById("mute-spotify");
+const key = "activate-extenstion"
+
+chrome.storage.local.get([key], (res) => {
+    const activate = res[key];
+    muteButton.innerHTML = `${activate === "true" ? "Activate" : "Deactivate"} Extension`;
+});
+
+muteButton.addEventListener("click", () => {
+    chrome.storage.local.get([key], (res) => {
+        const activate = res[key];
+        chrome.storage.local.set({ [key]: (activate === "true" ? "false" : "true") }, () => {
+            console.log(`mute-spotify-extenstion ${activate === "true" ? "deactivated" : "activated"}`);
+            muteButton.innerHTML = `${activate === "true" ? "Deactivate" : "Activate"} Extension`;
+        });
+    });
 })
