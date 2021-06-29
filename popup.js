38ca3b8a422441e16378b7e374f7536d2561f725
vivek9patel <vivek.p9737@gmail.com>
2021-06-29 21:26:41 +0530

    adds google analytics

--- popup.js
+++ popup.js
@@ -1,3 +1,16 @@
+import { googleAnalyticsCode } from './secrets.js';
+
+// google analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', googleAnalyticsCode]);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 const muteButton = document.getElementById("mute-spotify");
 const key = "activate-extenstion"
 
@@ -7,9 +20,11 @@ chrome.storage.local.get([key], (res) => {
 });
 
 muteButton.addEventListener("click", () => {
+    _gaq.push(['_trackEvent', 'mute.popup.button', 'clicked']);
     chrome.storage.local.get([key], (res) => {
         const activate = res[key];
         chrome.storage.local.set({ [key]: (activate === "true" ? "false" : "true") }, () => {
+            _gaq.push(['_trackEvent', `extension-toggled`, `${activate === "true" ? "deactivated" : "activated"}`]);
             console.log(`mute-spotify-extenstion ${activate === "true" ? "deactivated" : "activated"}`);
             muteButton.innerHTML = `${activate === "true" ? "Deactivate" : "Activate"} Extension`;
         });
---
+++ secrets.js
@@ -0,0 +1,1 @@
+export const googleAnalyticsCode = "UA-182113713-2"
